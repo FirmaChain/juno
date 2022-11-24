085ee20b249cb1ea10a4fc1752bd24875211789b
Donghoon Kim <fly33499@gmail.com>
2022-11-24 16:12:37 +0900

    fix : restore log

--- database/legacy/v3/migrate.go
+++ database/legacy/v3/migrate.go
@@ -106,7 +106,7 @@ func (db *Migrator) migrateTransactions(rows []types.TransactionRow, partitionSi
 			return fmt.Errorf("error while creating transaction partition table: %s", err)
 		}
 
-		//log.Debug().Int64("tx height", tx.Height).Msg("processing transactions")
+		log.Debug().Int64("tx height", tx.Height).Msg("processing transactions")
 
 		// Append params
 		params = append(params, tx.Hash, tx.Height, tx.Success, tx.Messages, tx.Memo, tx.Signatures,
@@ -126,7 +126,7 @@ func (db *Migrator) migrateTransactions(rows []types.TransactionRow, partitionSi
 	}
 
 	for _, tx := range rows {
-		//log.Debug().Int64("tx height", tx.Height).Msg("processing transaction messages")
+		log.Debug().Int64("tx height", tx.Height).Msg("processing transaction messages")
 
 		// Insert the messages of this transaction
 		err = db.insertTransactionMessages(tx, partitionSize)
